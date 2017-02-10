2434045d477486701cafafc1d4e235236abaaa89
victor.h.s.reis@gmail.com <victor.h.s.reis@gmail.com>
2017-02-10 20:54:12 -0200

    correção das rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,12 +1,12 @@
 from flask import render_template, url_for, request, redirect, flash
-from flask_login import login_manager, login_user, login_required, logout_user, current_user
+#from flask_login import login_manager, login_user, login_required, logout_user, current_user
 from app import app
 from app.models import tables
 from app import db
 
 db.create_all()
 
-@app.login_manager.user_loader
+"""@app.login_manager.user_loader
 def load_user(user_id):
     return tables.Users.query.get(int(user_id))
 
@@ -25,23 +25,21 @@ def login():
             return "Erro de senha"
         else:
             return redirect(url_for('home'))
-
-
+"""
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html",classe1="active")
 
 @app.route("/novo")
-@login_required
 def novochamado():
-    user = current_user.name # Nome de usuario será pego do loguin,talvez fique num cookei
+    user = "teste"# Nome de usuario será pego do loguin,talvez fique num cookei
     return render_template("novochamado.html",
                             user=user,
                             classe3="active")
 
 
 @app.route("/novo", methods=['GET', 'POST'])
-@login_required
 def addnovo():
     if request.method == "POST":
         chamado = request.form.get("chamado")
@@ -51,7 +49,6 @@ def addnovo():
         pedido_of = request.form.get("pedido_of")
         rota = request.form.get("rota")
         obs = request.form.get("obs")
-
         if chamado and data and rota and cidade and pedido:
             p = tables.Chamados(chamado,data,cidade, pedido, pedido_of, rota, obs)
             db.session.add(p)
@@ -70,8 +67,9 @@ def addnovo():
                 flash("Pedido não incluso.")
 
     return redirect(url_for("novochamado"))
+
+
 @app.route("/busca")
-@login_required
 def listar():
     chamados = tables.Chamados.query.all()
     return render_template("lista.html", chamados=chamados, classe2="active")
--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 DEBUG = True
-#user xama
-#senha =v$x6H]MXXP%
-
-SQLALCHEMY_DATABASE_URI = 'mysql://xama:=v$x6H]MXXP%@localhost/xama'
+#SQLALCHEMY_DATABASE_URI = 'mysql://xama:=v$x6H]MXXP%@localhost/xama'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///dados.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = "alice passou pelo espelho"
