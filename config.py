d5c1881f13743a52a2b9c508e7f87ba3a492669f
victor.h.s.reis@gmail.com <victor.h.s.reis@gmail.com>
2017-02-10 14:00:09 -0200

    modified:   app/templates/index.html 	modified:   config.py 	new file:   os

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 DEBUG = True
+#user xama
+#senha =v$x6H]MXXP%
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///dados.sqlite'
+SQLALCHEMY_DATABASE_URI = 'mysql://xama:=v$x6H]MXXP%@localhost/xama'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = "alice passou pelo espelho"
